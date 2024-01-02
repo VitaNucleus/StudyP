3ff977fdf2d2bf78340ed2b33ffbfdd74e7634c7
Daniil <batman1901@bk.ru>
2024-01-02 20:20:53 +0500

    02.01.2024 D Надо подружиться с smtp яндекса и чем-то, что не schedule

--- app.py
+++ app.py
@@ -1,10 +1,47 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, url_for
+from email.message import EmailMessage
+import schedule
+import smtplib
 import json
 import os
+import time
 
 
 app = Flask(__name__)
 
+
+def send_mail():
+    with open('cache/main.json', 'r') as file:
+        json_dict = json.load(file)
+
+        keys = list(json_dict.keys())
+        host_email = os.getenv('HOSTNAME')
+        host_pass = os.getenv('HOSTPASSWORD')
+        print('test')
+        with smtplib.SMTP_SSL('smtp.yandex.com') as smtp:
+            print('good')
+            smtp.login(host_email, host_pass)
+            for key in keys:
+                print('ok')
+                dest_email = json_dict[key]['e-mail']
+                subject = "Акты проверки нарушений качества теплоснабжения"
+
+                msg = EmailMessage()
+                msg["From"] = host_email  # Прозвище, учетная запись почтового ящика отправителя
+                msg["To"] = dest_email  # Прозвище, учетная запись почтового ящика получателя
+                msg["Subject"] = subject  # Тема письма
+                msg['Text'] = json_dict[key]['text']
+
+                smtp.send_message(msg)
+            smtp.quit()
+@app.route('/sended')
+def every_day():
+    schedule.every().minute.do(send_mail)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+    return render_template('sended.html')
 
 @app.route("/", methods=['GET', 'POST'])
 def make_record_in_json():
@@ -28,6 +65,8 @@ def make_record_in_json():
 
                     json_write = open('cache/main.json', 'w')
                     json.dump(json_dict, json_write, indent=4)
+
+                    redirect(url_for('every_day'))
                 else:
                     keys = list(json_dict.keys())
 
@@ -40,6 +79,8 @@ def make_record_in_json():
 
                     json_write = open('cache/main.json', 'w')
                     json.dump(new_dict, json_write, indent=4)
+
+                    redirect(url_for('every_day'))
             except:
                 records_dict = {}
                 records_dict['1'] = {'e-mail': e_mail,
@@ -47,6 +88,8 @@ def make_record_in_json():
 
                 json_write = open('cache/main.json', 'w')
                 json.dump(records_dict, json_write, indent=4)
+
+                redirect(url_for('every_day'))
 
     return render_template('main.html')
 
